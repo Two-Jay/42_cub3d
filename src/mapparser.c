4eeba0d006378496c11d086fa665769adbac5e25
Two-Jay <djeeee1272@gmail.com>
2021-10-25 04:14:35 +0900

    EDIT : added malloc error check logic in run_mapfile

--- src/mapparser.c
+++ src/mapparser.c
@@ -6,7 +6,7 @@
 /*   By: jekim <jekim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/25 02:58:51 by jekim             #+#    #+#             */
-/*   Updated: 2021/10/25 04:03:15 by jekim            ###   ########seoul.kr  */
+/*   Updated: 2021/10/25 04:13:47 by jekim            ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ char	*make_abspath(char *pwd, char *filepath, t_data *data)
 	pwd_l = ft_strlen(data->current_path);
 	fp_l = ft_strlen(filepath);
 	ret = (char *)ft_calloc(sizeof(char), pwd_l + fp_l);
+	if (!ret)
+		return (NULL);
 	ft_strlcpy(ret, pwd, pwd_l + 1);
 	while (*filepath == '.')
 		filepath++;
@@ -37,14 +39,15 @@ int	run_mapfile(char *filepath, t_data *data)
 
 	path_l = ft_strlen(filepath);
 	if (!ft_strequel(filepath + path_l - 4, ".cub"))
-		ft_strerr("Error : invaild map file\n");
+		return (-1);
+	target_path = filepath;
 	if (!ft_strncmp(filepath, "./", 2) || !ft_strncmp(filepath, "../", 3))
 		target_path = make_abspath(data->current_path, filepath, data);
-	else
-		target_path = filepath;
+	if (!target_path)
+		return (-1);
 	fd_check = open(target_path, O_RDONLY, NULL);
 	if (fd_check == -1)
-		ft_strerr("Error : invaild map file\n");
+		return (-1);
 	return (fd_check);
 }
 
@@ -52,8 +55,12 @@ int	parse_mapfile(char *filepath, t_data *data)
 {
 	int	map_fd;
 
-	trp(data);
 	map_fd = run_mapfile(filepath, data);
-	tri(map_fd);
-	return (0);
+	if (map_fd == -1)
+		ft_strerr("Error : map file error\n");
+	if (parse_imagepath(map_fd, data)
+		|| parse_RGB(map_fd, data)
+		|| parse_mapdata(map_fd, data))
+		return (close(map_fd) || 1);
+	return (close(map_fd));
 }
