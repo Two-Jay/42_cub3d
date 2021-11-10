ec765edfc28a4973c742945e28303553c8b2a354
KIM JEONGJUN <kimjeongjun@KIMs-MacBook-Pro.local>
2021-11-10 17:17:07 +0900

    ADD : complete validate_mapdata, complete mapparser v1.0

--- src/mapparser.c
+++ src/mapparser.c
@@ -6,7 +6,7 @@
 /*   By: jekim <arabi1549@naver.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/25 02:58:51 by jekim             #+#    #+#             */
-/*   Updated: 2021/11/10 17:04:53 by jekim            ###   ########seoul.kr  */
+/*   Updated: 2021/11/10 17:16:23 by jekim            ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -357,27 +357,61 @@ int check_mappoint(char **map, int ix, int jx)
 	return (0);
 }
 
+void count_space_type(char point, t_data *data)
+{
+	if (point == '0')
+		data->parsed_data.space_cnt++;
+	else if (point == 'N')
+		data->parsed_data.N_cnt++;
+	else if (point == 'S')
+		data->parsed_data.S_cnt++;
+	else if (point == 'E')
+		data->parsed_data.E_cnt++;
+	else if (point == 'W')
+		data->parsed_data.W_cnt++;
+}
+
+void set_count_bucket(t_data *data)
+{
+	data->parsed_data.space_cnt = 0;
+	data->parsed_data.N_cnt = 0;
+	data->parsed_data.S_cnt = 0;
+	data->parsed_data.E_cnt = 0;
+	data->parsed_data.W_cnt = 0;
+}
+
 int validate_mapdata_space(char **map, t_data *data)
 {
 	int checker;
-	int space_cnt;
 	int	ix;
 	int jx;
 
 	ix = 1;
+	set_count_bucket(data);
 	while (ix < data->parsed_data.map_height - 1)
 	{
 		jx = 1;
 		while (jx < data->parsed_data.map_width - 1)
 		{
 			if (ft_strchr(SPACE_CHARSET, map[ix][jx]))
+			{
+				count_space_type(map[ix][jx], data);
 				checker = check_mappoint(map, ix, jx);
+			}
 			if (checker)
 				return (1);
 			jx++;
 		}
 		ix++;
 	}
+	return (0);
+}
+
+int validate_mapdata_object(t_data *data)
+{
+	if (data->parsed_data.N_cnt == 0
+		|| data->parsed_data.space_cnt == 0)
+		return (1);
 	return (0);
 }
 
@@ -394,6 +428,7 @@ int	parse_mapfile(char *filepath, t_data *data)
 		|| convert_mapdata_matrix(data)
 		|| validate_mapdata_border(data->map_matrix, data)
 		|| validate_mapdata_space(data->map_matrix, data)
+		|| validate_mapdata_object(data)
 		|| clear_mapdata_lst(data->parsed_data.rawdata))
 		return (close(map_fd) || ft_strerr("Error : invalid map data\n"));
 	return (close(map_fd));
