ae9748ca798ec39b1c0ef71657e478af60035963
djeeee1272@gmail.com <djeeee1272@gmail.com>
2021-12-22 12:27:23 +0900

    check

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: jekim <arabi1549@naver.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/22 14:00:33 by jekim             #+#    #+#             */
-/*   Updated: 2021/12/20 11:01:05 by jekim            ###   ########.fr       */
+/*   Updated: 2021/12/20 17:24:52 by jekim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,83 +20,22 @@
 # include <math.h>
 # include "../lib/libft/libft.h"
 # include "../lib/minilibx_mms_20210621/mlx.h"
+# include "./structure.h"
 
 # define trs(x...) { printf("[%s:%d] %s = ", __func__, __LINE__, #x); printf("%s\n", x); }
 # define trc(x...) { printf("[%s:%d] %s = ", __func__, __LINE__, #x); printf("%c\n", x); }
 # define tri(x...) { printf("[%s:%d] %s = ", __func__, __LINE__, #x); printf("%d\n", x); }
 # define trp(x...) { printf("[%s:%d] %s = ", __func__, __LINE__, #x); printf("%p\n", x); }
 
+/*
+** key binding
+*/
 # define BUTTON_REDCROSS 0x6d357408
 # define KEY_ESC 0x35
 # define KEY_W 0xd
 # define KEY_S 0x1
 # define KEY_A 0x0
 # define KEY_D 0x2
-
-typedef struct s_mapdata_lst
-{
-	char *row;
-	struct s_mapdata_lst	*next;
-}	t_mapdata_lst;
-
-typedef struct s_player
-{
-	double pos_x;
-	double pos_y;
-	int	health;
-	int	speed;
-	int armo;
-}	t_player;
-
-typedef struct s_static
-{
-	char					*NO_img_filepath;
-	char					*SO_img_filepath;
-	char					*WE_img_filepath;
-	char					*EA_img_filepath;
-	int						*F_RGB;
-	int						F_RGB_value;
-	int						*C_RGB;
-	int						C_RGB_value;
-	int						map_width;
-	int						map_height;
-	int						space_cnt;
-	int						N_cnt;
-	int						S_cnt;
-	int						E_cnt;
-	int						W_cnt;
-	struct s_mapdata_lst	*rawdata;
-}	t_static;
-
-typedef struct	s_window
-{
-	int 			resol_max_x;
-	int 			resol_max_y;
-	int				resol_basic_x;
-	int				resol_basic_y;
-	void			*win_ptr;
-}	t_window;
-
-typedef struct s_img
-{
-	void	*main_image_ptr;
-	int		*main_image_data;
-	int		size_length;
-	int		bpp;
-	int		endian;
-}	t_img;
-
-typedef struct s_data
-{
-	void			*mlx_ptr;
-	char			*current_path;
-	int				**map_matrix;
-	struct s_player *player;
-	struct s_window	*window;
-	struct s_static *parsed_data;
-	struct s_img	*img;
-}	t_data;
-
 
 /*
 **	initiation_part
---
+++ includes/structure.h
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   structure.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jekim <arabi1549@naver.com>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/20 17:23:08 by jekim             #+#    #+#             */
+/*   Updated: 2021/12/20 17:23:52 by jekim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef STRUCTURE_H
+# define STRUCTURE_H
+
+typedef struct s_mapdata_lst
+{
+	char *row;
+	struct s_mapdata_lst	*next;
+}	t_mapdata_lst;
+
+typedef struct s_player
+{
+	double pos_x;
+	double pos_y;
+	int	health;
+	int	speed;
+	int armo;
+}	t_player;
+
+typedef struct s_static
+{
+	char					*NO_img_filepath;
+	char					*SO_img_filepath;
+	char					*WE_img_filepath;
+	char					*EA_img_filepath;
+	int						*F_RGB;
+	int						F_RGB_value;
+	int						*C_RGB;
+	int						C_RGB_value;
+	int						map_width;
+	int						map_height;
+	int						space_cnt;
+	int						N_cnt;
+	int						S_cnt;
+	int						E_cnt;
+	int						W_cnt;
+	struct s_mapdata_lst	*rawdata;
+}	t_static;
+
+typedef struct	s_window
+{
+	int 			resol_max_x;
+	int 			resol_max_y;
+	int				resol_basic_x;
+	int				resol_basic_y;
+	void			*win_ptr;
+}	t_window;
+
+typedef struct s_img
+{
+	void	*main_image_ptr;
+	int		*main_image_data;
+	int		size_length;
+	int		bpp;
+	int		endian;
+}	t_img;
+
+typedef struct s_data
+{
+	void			*mlx_ptr;
+	char			*current_path;
+	int				**map_matrix;
+	struct s_player *player;
+	struct s_window	*window;
+	struct s_static *parsed_data;
+	struct s_img	*img;
+}	t_data;
+
+#endif
