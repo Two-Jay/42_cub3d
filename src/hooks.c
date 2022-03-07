f0a4172c7172cb8596a1d220d019bdf95c31d068
gilee <gilee@student.42seoul.kr>
2022-03-07 20:11:44 +0900

    add move

--- src/hooks.c
+++ src/hooks.c
@@ -6,7 +6,7 @@
 /*   By: jekim <jekim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 16:08:01 by jekim             #+#    #+#             */
-/*   Updated: 2021/12/28 18:34:10 by jekim            ###   ########.fr       */
+/*   Updated: 2022/03/07 19:57:03 by gilee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,29 +22,62 @@ int subcallback_exit(void *dt)
 	return (0);
 }
 
-int subcallback_press_move(int code, void *dt)
+int subcallback_press_move(int code, t_data *data)
 {
-	t_data *data;
+	double	old_dirx;
+	double	old_planex;
+	static	int	frame;
+	double	rot_speed;
 
-	data = (t_data *)dt;
+	rot_speed = RAD * (frame * 0.3);
 	if (code == KEY_W)
-		printf ("cub3d : pressed W %d / key\n", code);
+	{
+		if (data->map->mtrx[(int)(data->camera->pos.y)][(int)(data->camera->pos.x + data->camera->dir.x * MOVE_SPEED)] != 2)
+			data->camera->pos.x += data->camera->dir.x * MOVE_SPEED;
+		if (data->map->mtrx[(int)(data->camera->pos.y + data->camera->dir.y * MOVE_SPEED)][(int)(data->camera->pos.x)] != 2)
+			data->camera->pos.y += data->camera->dir.y * MOVE_SPEED;
+	}
 	if (code == KEY_A)
-		printf ("cub3d : pressed A %d / key\n", code);
+	{
+		old_dirx = data->camera->dir.x;
+		data->camera->dir.x = data->camera->dir.x * cos(rot_speed) - data->camera->dir.y * sin(rot_speed);
+		data->camera->dir.y = old_dirx * sin(rot_speed) + data->camera->dir.y * cos(rot_speed);
+		printf("%lf %lf\n", data->camera->dir.x, data->camera->dir.y);
+		old_planex = data->camera->plain.x;
+		data->camera->plain.x = cos(rot_speed) - data->camera->plain.y * sin(rot_speed);
+		data->camera->plain.y = old_planex * sin(rot_speed) + data->camera->plain.y * cos(rot_speed);
+		printf("%lf %lf\n", data->camera->plain.x, data->camera->plain.y);
+	}
 	if (code == KEY_S)
-		printf ("cub3d : pressed S %d / key\n", code);
+	{
+		if (data->map->mtrx[(int)(data->camera->pos.y)][(int)(data->camera->pos.x - data->camera->dir.x * MOVE_SPEED)] != 2)
+			data->camera->pos.x -= data->camera->dir.x * MOVE_SPEED;
+		if (data->map->mtrx[(int)(data->camera->pos.y - data->camera->dir.y * MOVE_SPEED)][(int)(data->camera->pos.x)] != 2)
+			data->camera->pos.y -= data->camera->dir.y * MOVE_SPEED;
+	}
 	if (code == KEY_D)
-		printf ("cub3d : pressed D %d / key\n", code);
+	{
+		old_dirx = data->camera->dir.x;
+		printf("%lf\n", cos(-rot_speed));
+		data->camera->dir.x = data->camera->dir.x * cos(-rot_speed) - data->camera->dir.y * sin(-rot_speed);
+		data->camera->dir.y = old_dirx * sin(-rot_speed) + data->camera->dir.y * cos(-rot_speed);
+		printf("%lf %lf\n", data->camera->dir.x, data->camera->dir.y);
+		old_planex = data->camera->plain.x;
+		data->camera->plain.x = cos(-rot_speed) - data->camera->plain.y * sin(-rot_speed);
+		data->camera->plain.y = old_planex * sin(-rot_speed) + data->camera->plain.y * cos(-rot_speed);
+		printf("%lf %lf\n", data->camera->plain.x, data->camera->plain.y);
+	}
+	render(data);
+	frame++;
 	return (0);
 }
 
-int callback_key(int code, void *dt)
+int callback_key(int code, t_data *data)
 {
-	ft_printf("cub3d : pressed %x / key\n", code);
 	if (code == KEY_W || code == KEY_A || code == KEY_S || code == KEY_D)
-		subcallback_press_move(code, dt);
+		subcallback_press_move(code, data);
 	if (code == KEY_ESC)
-		subcallback_exit(dt);
+		subcallback_exit(data);
 	return (0);
 }
 
@@ -69,8 +102,9 @@ int callback_hook(char *str)
 void set_hooks(t_data *data)
 {
 	mlx_mouse_hook(data->window->win_ptr, &callback_mouse, "mouse");
-	mlx_key_hook(data->window->win_ptr, &callback_key, (void *)data);
-	mlx_expose_hook(data->window->win_ptr, &callback_expose, "expose");
-	mlx_hook(data->window->win_ptr, 17, 0, &subcallback_exit, (void *)data);
-	// mlx_loop_hook(data->mlx_ptr, &callback_hook, "loop");
+	//mlx_hook(data->window->win_ptr, 17, 0, &callback_key, data);
+	mlx_hook(data->window->win_ptr, EVENT_KEY_PRESS, 0, &callback_key, (void *)data);
+	//mlx_expose_hook(data->window->win_ptr, &callback_expose, "expose");
+	//mlx_hook(data->window->win_ptr, 17, 0, &subcallback_exit, (void *)data);
+	//mlx_loop_hook(data->window->mlx_ptr, render, data);
 }
